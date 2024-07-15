1275739afe09874d50e00a0ef466e97ba2b79650
yousuf hasan <hasanyousuf.ml.django@gmail.com>
2024-07-15 14:55:05 +0600

    first commit

---
+++ Weather project/weather_processor.py
@@ -0,0 +1,11 @@
+class WeatherProcessor:
+    def Menu_details(self):
+        user_choices=""" 
+        Enter:
+        -'a' to add a new book
+        -'l' to list all books
+        -'r' to mark a book as read
+        -'d' to delete a book
+         - 'q' to quit
+        """
+    
